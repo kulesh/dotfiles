c58f3f7a27b6111e09720e642dfced5d0fd9ee25
Kulesh Shanmugasundaram <kulesh@kulesh.org>
2025-03-15 20:45:47 -0400

    Markdown preview plugin for nvim

---
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,25 @@
+
+-- https://github.com/iamcco/markdown-preview.nvim
+
+local Plugin = {
+  'iamcco/markdown-preview.nvim',
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function()
+    -- Install without yarn or npm
+    vim.fn["mkdp#util#install"]()
+  end,
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  config = function()
+    -- Ensure the plugin's commands are registered
+    vim.cmd([[
+      command! -nargs=0 MarkdownPreview call mkdp#util#open_preview_page()
+      command! -nargs=0 MarkdownPreviewStop call mkdp#util#stop_preview()
+      command! -nargs=0 MarkdownPreviewToggle call mkdp#util#toggle_preview()
+    ]])
+  end
+}
+
+return Plugin
