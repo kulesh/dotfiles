82b5471af591ea42a3dfc4700e361f4ce3837871
Kulesh Shanmugasundaram <164083+kulesh@users.noreply.github.com>
2026-04-05 02:30:12 -0400

    Update tree-sitter for nvim 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,62 +1,62 @@
-local Plugin = {'nvim-treesitter/nvim-treesitter'}
+local Plugin = { "nvim-treesitter/nvim-treesitter", branch = "main" }
 
-Plugin.main = 'nvim-treesitter.configs'
+Plugin.main = "nvim-treesitter"
 
 Plugin.dependencies = {
-  {'nvim-treesitter/nvim-treesitter-textobjects'}
+	{ "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 }
 
 -- See :help nvim-treesitter-modules
 Plugin.opts = {
-  auto_install = true,
+	auto_install = false,
 
-  highlight = {
-    enable = true,
-  },
+	highlight = {
+		enable = true,
+	},
 
-  -- :help nvim-treesitter-textobjects-modules
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      }
-    },
-  },
+	-- :help nvim-treesitter-textobjects-modules
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+			},
+		},
+	},
 
-  ensure_installed = {
-		'vim',
-		'vimdoc',
-		'python',
-		'ruby',
-		'javascript',
-		'typescript',
-		'bash',
-		'c',
-		'diff',
-		'html',
-		'jsdoc',
-		'json',
-		'jsonc',
-		'lua',
-		'luadoc',
-		'luap',
-		'markdown',
-		'markdown_inline',
-		'printf',
-		'query',
-		'regex',
-		'toml',
-		'tsx',
-		'typescript',
-		'xml',
-		'yaml',
-  },
+	ensure_installed = {
+		"vim",
+		"vimdoc",
+		"python",
+		"ruby",
+		"javascript",
+		"typescript",
+		"bash",
+		"c",
+		"diff",
+		"html",
+		"jsdoc",
+		"json",
+		"jsonc",
+		"lua",
+		"luadoc",
+		"luap",
+		"markdown",
+		"markdown_inline",
+		"printf",
+		"query",
+		"regex",
+		"swift",
+		"toml",
+		"tsx",
+		"typescript",
+		"xml",
+		"yaml",
+	},
 }
 
 return Plugin
-
